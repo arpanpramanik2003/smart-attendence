01c65c473dcdecf6ec79db2fc79ae095e6310a5f
Arpan Pramanik <pramanikarpan089@gmail.com>
2025-07-26 23:46:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 if os.environ.get("FLASK_ENV") != "production":
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+   database_url = os.environ.get('DATABASE_URL')
+    if database_url and database_url.startswith('postgres://'):
+        database_url = database_url.replace('postgres://', 'postgresql://', 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_dev_secret_key')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
